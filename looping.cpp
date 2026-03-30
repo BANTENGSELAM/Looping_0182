29d28bfb53226021e326ca26cc970521e3616404
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-03-30 15:57:19 +0700

    untuk mendapatkan urutan angka acak yg berbeda

--- looping.cpp
+++ looping.cpp
@@ -16,4 +16,13 @@ int main(){
     cout<<endl;
     //untuk mendapatkan urutan angka acak yg berbeda
     srand (time(0));
+
+    //perulangan dengan WHILE
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"Bilangan Acak = "<<x <<endl;
+        x = rand()%10;
+    }
 }
