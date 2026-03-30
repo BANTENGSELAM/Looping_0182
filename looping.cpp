30534619761348a7618f757e038c5f9cb6ae0932
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-03-30 16:12:30 +0700

    logika 2

--- looping.cpp
+++ looping.cpp
@@ -92,4 +92,12 @@ int main (){
     cout<<"Masukkan Nilai Bahasa Inggris = ";
     cin >>nilB;
 
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) && (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak Lulus";
+
+    cout << "Status Kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
 }
