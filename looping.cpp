bf964b3b083edf2b4d8b7fe831da8ebb320ff3d3
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-03-30 16:09:56 +0700

    logika AND

--- looping.cpp
+++ looping.cpp
@@ -80,3 +80,16 @@ int main(){
         
     cout<<"Status Kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
 }
+
+//logika AND
+
+int main (){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan Nilai Matematika = ";
+    cin >>nilM;
+    cout<<"Masukkan Nilai Bahasa Inggris = ";
+    cin >>nilB;
+
+}
