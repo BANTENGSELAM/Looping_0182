217526ba462a2460983d09fdd58a4776c46a26b4
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-03-30 16:23:56 +0700

    prosedu

--- switchCase.cpp
+++ switchCase.cpp
@@ -38,3 +38,12 @@ void pilihanJurusan(){
             statusKuota = "Error: Kode Tidak Valid";
     }
 }
+
+//prosedu
+void output (){
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan    : " << namaJurusan << endl;
+    cout << "Status Kuota       : " << statusKuota << endl;
+    cout << "------------------------------------" << endl;
+}
+
